da2adc05ab63c8345660d41a0bfc733c181bca44
Claire Rhoda <clairerhoda@Claires-MacBook-Pro.local>
2022-09-23 15:55:53 -0700

    fake reservation steps added for test purposes

--- home.js
+++ home.js
@@ -1,9 +1,19 @@
 
 const createButton = document.getElementById("button-start");
+const cancelButton = document.getElementById("cancel-btn");
 
 createButton.addEventListener("click", (e) => {
     e.preventDefault();
     
     location.reload();
     location.href='login.html';
+    
 })
+
+cancelButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    location.reload();
+    location.href='login.html';
+})
+
