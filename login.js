71e355f441a304ac046d7ae5671c4cd8ba1793f8
Claire Rhoda <clairerhoda@Claires-MacBook-Pro.local>
2022-09-28 19:15:05 -0700

    database updated, reservation js post and get calls added

--- login.js
+++ login.js
@@ -1,13 +1,32 @@
 const loginButton = document.getElementById("login-btn");
+const xhr = new XMLHttpRequest()
+xhr.responseType = 'json'
 
+xhr.open('GET', 'http://localhost:3000/users')
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.response);
+    }
+}
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    
-    //if login success
-    location.reload();
-    location.href='reservation.html';
+    var username = document.getElementById("username-input").value;
+    var password = document.getElementById("password-input").value;
+    console.log(username, password);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.response);
+        }
+    }
 
+
+    //if login success
+    // location.reload();
+    // location.href='reservation.html';
+    
     //or
-    location.href='cancel.html';
+    // location.href='cancel.html';
 
 })
+
+xhr.send()
