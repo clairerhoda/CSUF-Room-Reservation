11b91cd3c6bd5388a40aea22d52df9dfd6c1208e
apuentes <adrian.puentes30@gmail.com>
2022-10-23 15:02:33 -0700

    modified login.js login.html login.css queries.js index.js to query db for  user input  creds and if match then redirect to reservations.html else alert user to re-enter creds

--- login.js
+++ login.js
@@ -2,41 +2,35 @@ const loginButton = document.getElementById("login-btn");
 const xhr = new XMLHttpRequest()
 xhr.responseType = 'json'
 
-xhr.open('GET', 'http://localhost:3000/users')
+xhr.open('GET', 'http://localhost:3000/users/apuentes1@csu.fullerton.edu/foobar')
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         console.log(this.response);
     }
 }
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     var username = document.getElementById("username-input").value;
     var password = document.getElementById("password-input").value;
     if(username && password){
-        pool.query('SELECT * FROM users WHERE email = $1 AND password_hash = $2', 
-        [username, password], (error, results) => {
-            if (error) {
-              throw error
+        const url = 'http://localhost:3000/users/'+username+'/'+password;
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+
+            if (this.readyState == 4 && this.status == 200) {
+                if(this.response.length>0){
+                    location.href='reservation.html';
+                }else {
+                    //location.href = 'login.html';
+                    const invalCredsMessage = document.getElementById("invalid-creds");
+                    invalCredsMessage.textContent = 'Invalid Username and/or Password'
+                }
+
             }
-            if(results){
-                this.response.redirect('/reservation')
-            }
-        })}
-    console.log(username, password);
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.response);
         }
-    }
 
-
-    //if login success
-    // location.reload();
-    // location.href='reservation.html';
-    
-    //or
-    // location.href='userPage.html';
+} 
 
 })
-
-xhr.send()
