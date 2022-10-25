3e0fc7111a9de998671b6912ed63f80c18323eb1
clairerhoda <clairehrhoda@gmail.com>
2022-10-25 13:42:37 -0700

    UTC/Local time dates fixed

--- calendar.js
+++ calendar.js
@@ -1,4 +1,10 @@
-let slideIndex = 1;
+/*
+    This JavaScript file is responsible for displaying the calendar
+    date selection for the user from today to 2 weeks in the future.
+ */
+
+var slideIndex = 1;
+
 // Next/previous controls
 function plusSlides(n) {
     if (slideIndex + n != 0 && slideIndex + n != 12) {
@@ -7,86 +13,110 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("calendar-option");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+    let i;
+    let slides = document.getElementsByClassName("calendar-option");
+    if (n > slides.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = slides.length;}
+    for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "flex";
-  slides[slideIndex].style.display = "flex";
-  slides[slideIndex+1].style.display = "flex";
-  slides[slideIndex+2].style.display = "flex";
-  slides[slideIndex+3].style.display = "flex";
+    }
+    slides[slideIndex-1].style.display = "flex";
+    slides[slideIndex].style.display = "flex";
+    slides[slideIndex+1].style.display = "flex";
+    slides[slideIndex+2].style.display = "flex";
+    slides[slideIndex+3].style.display = "flex";
+}
+
+var availableRooms = [];
+async function getRooms(studentCount) {
+    return new Promise(resolve => {
+        var times = [];
+        var address ='http://localhost:3000/rooms';
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `${address}/${studentCount}`);
+        xhr.responseType = 'json';
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                for (const s of this.response) {
+                resolve(availableRooms.push(s.room_id));
+                }
+            } 
+        }
+        xhr.send();
+    })
 }
 
 var dateDict = new Object();
 
 async function getReservations(startRange, endRange, reservationTime) {
     return new Promise(resolve => {
-    var times = [];
-    var address ='http://localhost:3000/reservations'
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 
-    `${address}/${startRange}/${endRange}/${reservationTime}`);
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            for (const s of this.response) {
-                var date = new Date(s.available_time)
-                var newDate = new Date(date.getTime()+
-                    date.getTimezoneOffset()*60*1000);
-                var offset = date.getTimezoneOffset() / 60;
-                var hours = date.getHours()
-                newDate.setHours(hours - offset);
-                times.push(newDate);
-            }
-            resolve(times)
-        } 
-    }
-    xhr.send();
-})
- }
+        var times = [];
 
-async function getCalendarDates (reservationTime ) {
+        var address ='http://localhost:3000/reservations';
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 
+        `${address}/${startRange}/${endRange}/${reservationTime}`);
+        xhr.responseType = 'json';
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                for (const s of this.response) {
+                    console.log(s.available_time)
+                    times.push(new Date(s.available_time));
+                }
+                resolve(times);
+            } 
+        }
+        xhr.send();
+    })
+}
+
+async function getCalendarDates(reservationTime, studentCount) {
     try {
-        var availableDateDay = new Date()
+        var availableDateDay = new Date();
         var availableDateDay14 = new Date(Date.now() + 12096e5);
         availableDateDay14.setHours(24,0,0,0);
 
         const dates = [];
-        const roomSmall = ["PLN-215", "PLN-311"];
-        const roomLarge = ["PLN-111", "PLN-112", "PLN-114", "PLN-312", "PLN-314"];
 
-        availableDateDay = availableDateDay.toISOString()
-        availableDateDay14 = availableDateDay14.toISOString()
+        // TODO: if room with same date is available, 
+        // make sure it shows in available reservation table
+        await getRooms(studentCount);
+        // console.log("fffff: ",  availableRooms);
+
+        availableDateDay = availableDateDay.toISOString();
+        availableDateDay14 = availableDateDay14.toISOString();
         var dayNum = 0;
         while (availableDateDay < availableDateDay14) {
-            var openTime = new Date(availableDateDay).setHours(7,0,0,0)
+            var openTime = new Date(availableDateDay).setHours(7,0,0,0);
             var closeTime = new Date(availableDateDay)
-                .setDate(new Date(availableDateDay).getDate() + 1)
-            closeTime = new Date(closeTime).setHours(0,0,0,0)
+                .setDate(new Date(availableDateDay).getDate() + 1);
+            closeTime = new Date(closeTime).setHours(0,0,0,0);
 
             var dayOfWeek = new Date(openTime)
                 .toLocaleDateString('en-us', {weekday: "short"});
 
             // opening and close times are different for week days and weekends
-            if (dayOfWeek == "Fri" || dayOfWeek == "Sat" || dayOfWeek == "Sun") {
-                closeTime = new Date(openTime).setHours(17,0,0,0)
+            if (dayOfWeek == "Fri" || 
+                dayOfWeek == "Sat" || 
+                dayOfWeek == "Sun") {
+                closeTime = new Date(openTime).setHours(17,0,0,0);
             }
+
             if (dayOfWeek == "Sat" || dayOfWeek == "Sun") {
-                openTime = new Date(availableDateDay).setHours(9,0,0,0)
+                openTime = new Date(availableDateDay).setHours(9,0,0,0);
             }
-            //see if current date is still open, if not skip to tomorrow
+
+            //see if current date is still open, 
+            //if not skip to tomorrow
             if (new Date().toISOString() > new Date(closeTime).toISOString()) {
                 availableDateDay = 
                 new Date(availableDateDay)
                     .setDate(new Date(availableDateDay).getDate() + 1);
-                 availableDateDay = new Date(availableDateDay).toISOString()
+                 availableDateDay = new Date(availableDateDay).toISOString();
                 dayNum++;
                 continue;
             }
+
             // if time now is past regular open hours
             // change to nearest next 30 min time
             if (new Date().toISOString() > new Date(openTime).toISOString()) {
@@ -101,26 +131,26 @@ async function getCalendarDates (reservationTime ) {
                 reservationTime
             );
 
-            console.log(availableTimes.length)
+            // if no times available, do not show date
             if (availableTimes.length == 0) {
                 availableDateDay = 
                 new Date(availableDateDay)
                     .setDate(new Date(availableDateDay).getDate() + 1);
-                availableDateDay = new Date(availableDateDay).toISOString()
+                availableDateDay = new Date(availableDateDay).toISOString();
                 dayNum++;
-                continue
+                continue;
             }
 
-            dateDict[dayNum] = await availableTimes
+            dateDict[dayNum] = await availableTimes;
             dates.push(new Date(availableDateDay));
-        
             availableDateDay = 
                 new Date(availableDateDay)
                     .setDate(new Date(availableDateDay).getDate() + 1);
-            availableDateDay = new Date(availableDateDay).toISOString()
+            availableDateDay = new Date(availableDateDay).toISOString();
             dayNum++;
         }
 
+        // day selection from 2 week span
         var calendarList = document.createElement("div");
         calendarList.setAttribute("id", "calendar-list");
         for (let i = 0; i < dates.length; i++) {
@@ -130,10 +160,12 @@ async function getCalendarDates (reservationTime ) {
             var part2 = document.createElement("div");
             part1.style.pointerEvents = "none";
             part2.style.pointerEvents = "none";
-            var dateOptions = {  month: 'short', day: 'numeric', };
+            var dateOptions = {  month: 'short', day: 'numeric' };
             var dayOfWeekName = {weekday: 'short'};
-            part1.textContent = dates[i].toLocaleString('default', dayOfWeekName)
-            part2.textContent = dates[i].toLocaleDateString('en-us', dateOptions);
+            part1.textContent = dates[i].toLocaleString(
+                'default', dayOfWeekName);
+            part2.textContent = dates[i].toLocaleDateString(
+                'en-us', dateOptions);
             // trigger first date to be selected
             calendarOption.appendChild(part1);
             calendarOption.appendChild(part2);
@@ -144,18 +176,15 @@ async function getCalendarDates (reservationTime ) {
             }
         }
         var dateStart = 
-            document.getElementById("date-start").appendChild(calendarList)
-        document.getElementById("date-selection-table").appendChild(dateStart)
+            document.getElementById("date-start").appendChild(calendarList);
+        document.getElementById("date-selection-table").appendChild(dateStart);
         showSlides(slideIndex);
 
         
         var timeSelection = document.createElement("div");
         timeSelection.setAttribute("id", "time-selection");
         const objEntries = Object.entries(await dateDict);
-        var val = Object.values(dateDict)[0]
-        if(val.length == 0) {
-            console.log("null")
-        }
+        var val = Object.values(dateDict)[0];
         addTimesForDate(val);
            
         async function addTimesForDate(val) {
@@ -165,6 +194,7 @@ async function getCalendarDates (reservationTime ) {
                 timeSelection.removeChild(child);
                 child = timeSelection.lastElementChild;
             }
+
             // display available times for date selected
             for (var prop in await val) {
                 var timeSlot = document.createElement("div");
@@ -173,9 +203,9 @@ async function getCalendarDates (reservationTime ) {
                 var end = new Date(new Date(val[prop]).getTime() +
                     parseInt(reservationTime)*60000);
                 var meetingStart = start.toLocaleTimeString(
-                    'en-us', { hour:"numeric", minute:"numeric"})
+                    'en-us', { hour:"numeric", minute:"numeric"});
                 var meetingEnd = end.toLocaleTimeString(
-                    'en-us', { hour:"numeric", minute:"numeric"})
+                    'en-us', { hour:"numeric", minute:"numeric"});
                 timeSlot.textContent = meetingStart + " to " + meetingEnd;
                 var selectTimeBtn = document.createElement("div");
                 selectTimeBtn.setAttribute("id", "select-time-btn");
@@ -186,17 +216,19 @@ async function getCalendarDates (reservationTime ) {
                 timeRow.appendChild(timeSlot);
                 timeRow.appendChild(selectTimeBtn);
                 timeRow.setAttribute("id", start.getTime());
-
                 timeSelection.appendChild(timeRow);
             }
         }
        
-        document.getElementById("date-selection-table").appendChild(timeSelection)
+        document.getElementById("date-selection-table")
+            .appendChild(timeSelection);
        
+        // detect when reservation time has been selected
         timeSelection.addEventListener("click", (event) => {
+            
             if (event.target.tagName == "LI") {
                 var rows = document.querySelectorAll(".time-row");
-                var status = document.querySelectorAll("#select-time-btn") ;
+                var status = document.querySelectorAll("#select-time-btn");
                 [].forEach.call(rows, function(div) {
                     div.style.boxShadow = "none";
                     div.style.backgroundColor = "#e0e5ec";
@@ -206,15 +238,17 @@ async function getCalendarDates (reservationTime ) {
                     div.style.height = "35px";
                     div.textContent = "Select";
                 });               
-                event.target.style.boxShadow = "0px 0px 0px 2.5px #527496 inset";
+                event.target.style.boxShadow = 
+                    "0px 0px 0px 2.5px #527496 inset";
                 event.target.style.backgroundColor = "white";
-                var child = event.target.querySelectorAll('#select-time-btn')
+                var child = event.target.querySelectorAll('#select-time-btn');
                 child[0].style.width = "30px";
                 child[0].style.height = "30px";
                 child[0].textContent = "✓";
             }
         })
 
+        // detect when day from 2 week span has been selected
         calendarList.addEventListener("click", (event) => {
             if (event.target.tagName == "LI") {
                 var divs = document.querySelectorAll(".calendar-option");
@@ -227,9 +261,8 @@ async function getCalendarDates (reservationTime ) {
         })
 
         getCalendarDates = function(){};
-        
 
     } catch(error) {
-        //error
+        console.log(error);
     }
 }
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./queries');
+const port = 3000;
 
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -16,21 +16,22 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({ info: 'Node.js, Express, and Postgres API' });
 })
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.get('/rooms', db.getAllRooms)
-app.get('/rooms/:capacity', db.getAvailableRooms)
-app.get('/rooms/:id', db.getRoomById)
-app.post('/rooms', db.createRoom)
-app.get('/reservations/:userId', db.getReservationsByUser)
-app.post('/reservations/', db.createReservation)
-app.put('/reservations/:id', db.updateReservation)
-app.get('/reservations/:startRange/:endRange/:reservationTime', db.checkReservationAvailability)
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.get('/rooms', db.getAllRooms);
+app.get('/rooms/:capacity', db.getAvailableRooms);
+app.get('/rooms/:id', db.getRoomById);
+app.post('/rooms', db.createRoom);
+app.get('/reservations/:userId', db.getReservationsByUser);
+app.post('/reservations/', db.createReservation);
+app.put('/reservations/:id', db.updateReservation);
+app.get('/reservations/:startRange/:endRange/:reservationTime',
+ db.checkReservationAvailability);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
+  console.log(`App running on port ${port}.`);
 })
