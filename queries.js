a8ae709b41df63f8d8cd2ffeb8729f15e58d1b9f
clairerhoda <clairehrhoda@gmail.com>
2022-11-14 14:18:53 -0800

    listening port changed in indexRest

--- queries.js
+++ queries.js
@@ -9,6 +9,14 @@ const Pool = require('pg').Pool
 const dotenv = require('dotenv');
 dotenv.config();
 
+// const pool = new Pool({
+//   host: "peanut.db.elephantsql.com",
+//   user: "vgghswft",
+//   database: "vgghswft",
+//   password: "VMe8fbDYYhaemZAiKxqCiXIo4La9335z",
+//   port: 5432,
+// });
+
 // render database
 const pool = new Pool({
   host: process.env.DB_HOST,
